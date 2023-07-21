6d1acc57bd228a5bd9bb3ba2a57fdd30d673cc8b
Jatin Mahawar <131634000+jatinsm2023@users.noreply.github.com>
2023-07-21 15:57:59 +0530

    Update about.js

--- about.js
+++ about.js
@@ -5,7 +5,7 @@ function Navbar(mode) {
     let html = `
     <nav class="navbar navbar-expand-lg navbar-${mode} bg-${mode}" id="Navbar">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/index.html">Todo App</a>
+            <a class="navbar-brand" href="index.html">Todo App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -14,7 +14,7 @@ function Navbar(mode) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link"  href="about.html">About</a>
